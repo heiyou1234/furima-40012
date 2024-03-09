708c36c3e6fff1d7fb17ba087b40e48f8f95873a
heiyou1234 <maji.dousuru46@gmail.com>
2024-03-10 08:25:19 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :number_of_day
+  validates :number_of_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  belongs_to :prefecture
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  belongs_to :delivery
+  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  belongs_to :condition
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  belongs_to :category
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+end
+
 end
